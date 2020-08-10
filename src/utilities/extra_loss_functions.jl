93ac46b37ef3e1d917b8cedaa59603a991415e16
Dhruva2 <dhruvaraman@hotmail.com>
2020-08-10 18:34:32 +0100

    added inbounds to build_injection_loss for loop

--- src/utilities/extra_loss_functions.jl
+++ src/utilities/extra_loss_functions.jl
@@ -18,7 +18,7 @@ function build_injection_loss(prob::ODEProblem, solmethod::T, tpoints) where T <
         du_nom = similar(prob.u0, promote_type(eltype(prob.u0), eltype(p)))
         du_p = similar(pprob.u0, promote_type(eltype(pprob.u0), eltype(p)))
         c = 0.
-        for i = 1:n
+        @inbounds for i = 1:n
             prob.f(du_nom,nom_sol[:,i], prob.p, tpoints[i])
             pprob.f(du_p, nom_sol[:,i], p, tpoints[i])
             c += sum(abs2, du_nom .- du_p) 

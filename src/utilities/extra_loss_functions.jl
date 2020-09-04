318a7f05f2080dda7621972ff019bc6b961387ac
Dhruva2 <dhruvaraman@hotmail.com>
2020-09-04 16:13:54 +0100

    added new build_injection_loss method, for the case of an output map on the state trajectory

--- src/utilities/extra_loss_functions.jl
+++ src/utilities/extra_loss_functions.jl
@@ -5,17 +5,19 @@ The two stage method in DiffEqParamEstim is great, but it employs (intelligent) 
 
 """
 For a prob::ODEProblem, with nominal parameters p0, creates a cost function C(p) 
+Let pprob = remake(prob, p=p).
+C(p) is the collocation cost associated with  pprob. Calculated by integrating the following over the trajectory of solve(prob; saveat=tsteps):
 
-C(p) is the two stage method (collocation) cost associated with  remake(prob::ODEProblem; p=p), described in the DiffEqParamEstim.jl docs. In this case, the 'data' is solve(prob).
+ int_u  sum(pprob.f(u) - prob.f(u)).^2
+
+ with output map g(x), this turns into 
+
+ int_u  sum(dgdx(u)*pprob.f(u) - dgdx(u)*prob.f(u)).^2
 """
-function build_injection_loss(prob::ODEProblem, solmethod::T, tpoints, idxs=nothing) where T <: DiffEqBase.AbstractODEAlgorithm
+function build_injection_loss(prob::ODEProblem, solmethod::T, tpoints) where T <: DiffEqBase.AbstractODEAlgorithm
     pdim = length(prob.u0)
     nom_sol = Array(solve(prob, solmethod, saveat=tpoints))
     n = length(tpoints)
-    
-    if idxs===nothing
-        idxs=1:pdim
-    end
 
     function cost(p)
         pprob = remake(prob, p=p)
@@ -25,7 +27,40 @@ function build_injection_loss(prob::ODEProblem, solmethod::T, tpoints, idxs=noth
         @inbounds for i = 1:n
             prob.f(du_nom,nom_sol[:,i], prob.p, tpoints[i])
             pprob.f(du_p, nom_sol[:,i], p, tpoints[i])
-            c += sum(abs2, du_nom[idxs] .- du_p[idxs]) 
+            c += sum(abs2, du_nom .- du_p) 
+        end
+        return c
+    end
+
+    function cost2(p,g)
+        g[:] = ForwardDiff.gradient(cost, p)
+        return cost(p)
+    end
+    return DiffCost(cost, cost2)
+end
+
+"""
+alternative method with output_map g(x)
+
+"""
+function build_injection_loss(prob::ODEProblem, solmethod::T, tpoints, output_map= x -> x) where T <: DiffEqBase.AbstractODEAlgorithm
+    pdim = length(prob.u0)
+    nom_sol = Array(solve(prob, solmethod, saveat=tpoints))
+    n = length(tpoints)
+    dgdx = x -> ForwardDiff.jacobian(output_map, x)
+    dgdx_template = dgdx(prob.u0)
+    dgdx_template2 = deepcopy(dgdx_template)
+
+    function cost(p)
+        pprob = remake(prob, p=p)
+        du_nom = similar(prob.u0, promote_type(eltype(prob.u0), eltype(p)))
+        du_p = similar(pprob.u0, promote_type(eltype(pprob.u0), eltype(p)))
+        c = 0.
+        @inbounds for i = 1:n
+            prob.f(du_nom,nom_sol[:,i], prob.p, tpoints[i])
+            pprob.f(du_p, nom_sol[:,i], p, tpoints[i])
+            dgdx_template = dgdx(nom_sol[:,i])
+            c += sum(abs2, dgdx_template*du_nom .- dgdx_template*du_p) 
         end
         return c
     end

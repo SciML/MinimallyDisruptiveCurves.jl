b6a6b374e439b9ddf66b686cf1b94b4b11bf53aa
Dhruva2 <dhruvaraman@hotmail.com>
2022-03-08 18:43:49 +0000

    fixed lack of thread safety in two sided evolve()

--- src/evolve.jl
+++ src/evolve.jl
@@ -3,35 +3,29 @@
 Evolves a minimally disruptive curve, with curve parameters specified by curveProblem. Uses DifferentialEquations.solve() to run the ODE.
 MinimallyDisruptiveCurves.jl callbacks go in the `mdc_callback` keyword argument. You can also use any DifferentialEquations.jl callbacks compatible with DifferentialEquations.solve(). They go in the `callback` keyword argument
 """
-function evolve(c::CurveProblem, solmethod=Tsit5; mdc_callback=CallbackCallable[], callback=nothing, momentum_tol=1e-3,kwargs...) 
-    
+function evolve(c::CurveProblem, solmethod = Tsit5; mdc_callback = CallbackCallable[], callback = nothing, momentum_tol = 1e-3, kwargs...)
+
     (!(eltype(mdc_callback) == CallbackCallable)) && (mdc_callback = convert(Vector{CallbackCallable}, mdc_callback))
 
-    function merge_sols(neg, pos, p)
-        t = cat(neg.t, pos.t, dims=1)
-        u = cat(neg.u, pos.u, dims=1)
-        return DiffEqBase.build_solution(p, solmethod, t, u)
+    function merge_sols(ens, p)
+        if length(ens) == 1
+            return ens[1]
+        elseif length(ens) == 2
+            t = cat(-ens[1].t[end:-1:1], ens[2].t, dims = 1)
+            u = cat(ens[1].u[end:-1:1], ens[2].u, dims = 1)
+            return DiffEqBase.build_solution(p, Tsit5(), t, u)
+        end
     end
-    
+
     probs = c()
-    
+    prob_func = (prob, i, repeat) -> probs[i]
 
     callbacks = CallbackSet(
         build_callbacks(c, mdc_callback, momentum_tol)...,
         build_callbacks(c, callback)
     )
 
-    runn(p) = solve(p, solmethod(); callback=callbacks, kwargs...)
-    (length(probs) == 1) && (sols = runn(probs[1]))
-
-    if length(probs) == 2
-        psol = Threads.@spawn runn(probs[2])
-        nsol = runn(probs[1])
-        nsol.u[:] = nsol.u[end:-1:1]
-        nsol.t[:] = -nsol.t[end:-1:1]
-        wait(psol)
-        psol = psol.result
-        sols = merge_sols(nsol, psol, probs[end])
-    end
-    return MDCSolution(sols, c.cost)
+    e = EnsembleProblem(probs[1], prob_func = prob_func)
+    sim = solve(e, Tsit5(), EnsembleThreads(), trajectories = length(probs); callback=callbacks, kwargs...)
+    return merge_sols(sim, probs[1]) |> MDCSolution
 end
